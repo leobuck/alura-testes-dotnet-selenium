34cebd75e9bec44e33892909fbf9019e9c15d275
leobuck <leobuck12@gmail.com>
2023-12-17 18:28:03 -0300

    Capturando elementos da página

--- Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
+++ Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -50,5 +50,21 @@ namespace Alura.ByteBank.WebApp.Testes
 			driver.FindElement(By.CssSelector(".btn")).Click();
 			driver.Close();
 		}
+
+		[Fact]
+		public void ValidaLinkDeLoginNaHome()
+		{
+			// Arrange
+			IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+			driver.Navigate().GoToUrl("https://localhost:44309/");
+
+			var linkLogin = driver.FindElement(By.LinkText("Login"));
+
+			// Act
+			linkLogin.Click();
+
+			// Assert
+			Assert.Contains("img", driver.PageSource);
+		}
 	}
 }
