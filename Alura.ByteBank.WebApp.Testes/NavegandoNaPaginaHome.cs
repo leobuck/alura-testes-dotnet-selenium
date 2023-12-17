f384660f5af25667dcc0cfc7a87a3e26c7c11f04
leobuck <leobuck12@gmail.com>
2023-12-17 15:13:44 -0300

    Criando primeiro teste

---
+++ Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Alura.ByteBank.WebApp.Testes
+{
+	public class NavegandoNaPaginaHome
+	{
+		[Fact]
+		public void CarregaPaginaHomeEVerificaTituloDaPagina()
+		{
+			// Arrange
+			IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+			// Act
+			driver.Navigate().GoToUrl("https://localhost:44309");
+
+			// Assert
+			Assert.Contains("WebApp", driver.Title);
+		}
+
+		[Fact]
+		public void CarregadaPaginaHomeVerificaExistenciaLinkLoginEHome()
+		{
+			// Arrange
+			IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+			// Act
+			driver.Navigate().GoToUrl("https://localhost:44309");
+
+			// Assert
+			Assert.Contains("Login", driver.PageSource);
+			Assert.Contains("Home", driver.PageSource);
+		}
+	}
+}
