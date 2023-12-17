ca405de13a2f7a84109002d45af9a84ad4f42351
leobuck <leobuck12@gmail.com>
2023-12-17 18:44:20 -0300

    Testando o login da aplicação

---
+++ Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
@@ -0,0 +1,70 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Alura.ByteBank.WebApp.Testes
+{
+	public class AposRealizarLogin
+	{
+		[Fact]
+		public void AposRealizarLoginVerificaSeExisteOpcaoAgenciaMenu()
+		{
+			// Arrange
+			IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+			driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+			var login = driver.FindElement(By.Id("Email"));
+			var senha = driver.FindElement(By.Id("Senha"));
+			var btnLogar = driver.FindElement(By.Id("btn-logar"));
+
+			login.SendKeys("andre@email.com");
+			senha.SendKeys("senha01");
+
+			// Act
+			btnLogar.Click();
+
+			// Assert
+			Assert.Contains("Agência", driver.PageSource);
+		}
+
+		[Fact]
+		public void TentaRealizarLoginSemPreencherCampos()
+		{
+			// Arrange
+			IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+			driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+			var btnLogar = driver.FindElement(By.Id("btn-logar"));
+
+			// Act
+			btnLogar.Click();
+
+			// Assert
+			Assert.Contains("The Email field is required.", driver.PageSource);
+			Assert.Contains("The Senha field is required.", driver.PageSource);
+		}
+
+		[Fact]
+		public void TentaRealizarLoginComSenhaInvalida()
+		{
+			// Arrange
+			IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+			driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+			var login = driver.FindElement(By.Id("Email"));
+			var senha = driver.FindElement(By.Id("Senha"));
+			var btnLogar = driver.FindElement(By.Id("btn-logar"));
+
+			login.SendKeys("andre@email.com");
+			senha.SendKeys("senha010");
+
+			// Act
+			btnLogar.Click();
+
+			// Assert
+			Assert.Contains("Login", driver.PageSource);
+		}
+	}
+}
